302251cc208d88db51d164e818f233beed3c2a62
Kit Macleod <kitmacleod@gmail.com>
2018-10-09 13:50:20 +0100

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry:'./src/main.js',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    devtool: isProduction ? 'source-map':'cheap-module-eval-source-map',
+    devServer:{
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath:'/dist/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }]
+    },
+    plugins: [
+      new CopyPlugin([{from: 'data', to: 'data'}])
+    ]
+  };
+};
+
