7f7827a41bbb1d735396c074dd07537cc3b55a51
Kit Macleod <kitmacleod@gmail.com>
2018-10-10 12:49:20 +0100

    Adding html and scss styles with basic MBGL example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import mapboxgl from 'mapbox-gl';
+import'./styles/styles.scss';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2l0bWFjbGVvZCIsImEiOiJjam12d3F0Y3QyZXBvM3ZwbjRsajMwZG53In0.XU1YIWr-iN8_tovQx93X7A';
 const map = new mapboxgl.Map({
-  container: 'map',
+  container: 'map-container',
   style: 'mapbox://styles/mapbox/streets-v9',
   center: [-74.50, 40],
   zoom: 9
