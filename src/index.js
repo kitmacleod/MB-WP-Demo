d6d45e3692beccf240f2dce4c89a455613168db4
Kit Macleod <kitmacleod@gmail.com>
2018-10-31 11:57:15 +0000

    Trying to get turf.area returned to div in outcomes, console.logs OK

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import MapboxDraw from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw';
 // var MapboxDraw = require('@mapbox/mapbox-gl-draw');
 
 // Not sure how to import turf here (need it for MB example and other work)
-import turf from 'turf';
+// import turf from 'turf';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2l0bWFjbGVvZCIsImEiOiJjam12d3F0Y3QyZXBvM3ZwbjRsajMwZG53In0.XU1YIWr-iN8_tovQx93X7A';
 const map = new mapboxgl.Map({
@@ -34,14 +34,28 @@ map.on('draw.update', updateArea);
 // Need to check this, as not using <p>
 function updateArea(e) {
   var data = draw.getAll();
-  var answer = document.getElementById('calculated-area');
+  console.log(data);
+  var area = turf.area(data);
+  console.log(area);
+
+
+// Still working on getting 'area' returned to html element, see notes
+
+  var answer = document.getElementById('outcomes-area');
   if (data.features.length > 0) {
     var area = turf.area(data);
     var rounded_area = Math.round(area*100)/100;
-    answer.innerHTML = '<p><strong>' + rounded_area + '</strong></p><p>square meters</p>';
+    console.log(area);
+    console.log(rounded_area);
+    return `Rounded area ${area} square meters`;
+
+
+    // answer.innerHTML = '<p><strong>' + rounded_area + '</strong></p><p>square meters</p>';
+
   } else {
-      answer.innerHTML = '';
-      if (e.type !== 'draw.delete') alert("Use the draw tools to draw a polygon!");
+      console.log('error');
+     answer.innerHTML = '';
+       if (e.type !== 'draw.delete') alert("Use the draw tools to draw a polygon!");
   }
 }
 
