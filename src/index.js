db289386ea4f15bba0f2cfc8cee9df0052ce53c7
Kit Macleod <kitmacleod@gmail.com>
2018-11-12 10:12:31 +0000

    Added an example of using draw and queryRenderedFeatures- works

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import {html, render} from 'lit-html';
 
 import intersect from '@turf/intersect';
 import polygon from '@turf/helpers';
+import bbox from '@turf/bbox';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2l0bWFjbGVvZCIsImEiOiJjam12d3F0Y3QyZXBvM3ZwbjRsajMwZG53In0.XU1YIWr-iN8_tovQx93X7A';
 const map = new mapboxgl.Map({
@@ -65,176 +66,243 @@ const map = new mapboxgl.Map({
   }, 'place-city-sm');
 });
 
-var draw = new MapboxDraw({
+
+
+// Charlie example of draw to select polygons
+// Need to add Draw
+const draw = new MapboxDraw({
   displayControlsDefault: false,
   controls: {
     polygon: true,
     trash: true
-  } 
+  }
 });
-
-// Add zoom and rotation controls to the map
-map.addControl(new mapboxgl.NavigationControl());
 map.addControl(draw);
 
+// Charlie's function
+map.on('draw.create', function(el) {
+   let userPolygon = el.features[0];
+   console.log(userPolygon);
+
+   // Generate bbox from polygon
+   let polygonBoundingBox = turf.bbox(userPolygon);
+   console.log(polygonBoundingBox);
+
+   let southWest = [polygonBoundingBox[0], polygonBoundingBox[1]];
+   let northEast =[polygonBoundingBox[2], polygonBoundingBox[3]];
+
+   let northEastPointPixel = map.project(northEast);
+   let southWestPointPixel = map.project(southWest);
+   console.log(northEastPointPixel);
+
+   let features = map.queryRenderedFeatures([southWestPointPixel, northEastPointPixel], { layers: ['counties'] });
+
+   let filter = features.reduce(function(memo, feature) {
+     if (! (undefined === turf.intersect(feature, userPolygon))) {
+       // only add the property, if the feature intersects with drawn polygon
+       memo.push(feature.properties.FIPS);
+     }
+     return memo;
+   }, ['in', 'FIPS']);
+
+   map.setFilter("counties-highlighted", filter);
 
 
-// Add a MB layer for contours
-map.on('load', function() {
-  map.addLayer({
-    id: 'terrain-data',
-    type: 'line',
-    source: {
-      type: 'vector',
-      url: 'mapbox://mapbox.mapbox-terrain-v2'
-    },
-    'source-layer': 'contour'
-
-  });
 });
 
 
-// Add MB layer for landuse (not fully working)
+
+// Rest of MB example code to select features aroud a click using bbox (working)
+// map.on('click', function(e) {
+//   // set bbox as 5px rectangle around clicked point
+//   const bbox = [[e.point.x - 5, e.point.y - 5], [e.point.x + 5, e.point.y + 5]];
+//   const features = map.queryRenderedFeatures(bbox, { layers: ['counties'] });
+//   // Run through selected features and set a filter
+//   // to match features with unique FIPS codes to activate
+//   // the 'counties-highlighted' layer
+//   console.log('features', features);
+//   const filter = features.reduce(function(memo, feature) {
+//     memo.push(feature.properties.FIPS);
+//     return memo;
+//     console.log('memo', memo);
+//   },['in', 'FIPS']);
+//   map.setFilter("counties-highlighted", filter);
+// });
+
+
+
+
+// // Start of draw (working)
+// var draw = new MapboxDraw({
+//   displayControlsDefault: false,
+//   controls: {
+//     polygon: true,
+//     trash: true
+//   } 
+// });
+
+// // Add zoom and rotation controls to the map
+// map.addControl(new mapboxgl.NavigationControl());
+// map.addControl(draw);
+
+
+
+// // Add a MB layer for contours
 // map.on('load', function() {
 //   map.addLayer({
-//     id: 'terrain-land', 
-//     type: 'fill',
+//     id: 'terrain-data',
+//     type: 'line',
 //     source: {
 //       type: 'vector',
 //       url: 'mapbox://mapbox.mapbox-terrain-v2'
 //     },
-//     'source-layer': 'landcover'
+//     'source-layer': 'contour'
+
 //   });
 // });
 
 
-// Add improved hillshading based on MBGL example (may add again)
-// map.on('load', function () {
-//   map.addSource('dem', {
-//       "type": "raster-dem",
-//       "url": "mapbox://mapbox.terrain-rgb"
-//   });
-//   map.addLayer({
-//       "id": "hillshading",
-//       "source": "dem",
-//       "type": "hillshade"
-//   // insert below waterway-river-canal-shadow;
-//   // where hillshading sits in the Mapbox Outdoors style
-//   }, 'waterway-river-canal-shadow');
-// });
+// // Add MB layer for landuse (not fully working)
+// // map.on('load', function() {
+// //   map.addLayer({
+// //     id: 'terrain-land', 
+// //     type: 'fill',
+// //     source: {
+// //       type: 'vector',
+// //       url: 'mapbox://mapbox.mapbox-terrain-v2'
+// //     },
+// //     'source-layer': 'landcover'
+// //   });
+// // });
+
+
+// // Add improved hillshading based on MBGL example (may add again)
+// // map.on('load', function () {
+// //   map.addSource('dem', {
+// //       "type": "raster-dem",
+// //       "url": "mapbox://mapbox.terrain-rgb"
+// //   });
+// //   map.addLayer({
+// //       "id": "hillshading",
+// //       "source": "dem",
+// //       "type": "hillshade"
+// //   // insert below waterway-river-canal-shadow;
+// //   // where hillshading sits in the Mapbox Outdoors style
+// //   }, 'waterway-river-canal-shadow');
+// // });
 
 
 
-// Adds a WMS (tested, maybe modify later)
-// Use MBGL exmaple https://www.mapbox.com/mapbox-gl-js/example/wms/
-// map.on('load', function() {
-// map.addLayer({
-//   'id':'wms-test-layer',
-//   'type': 'raster',
-//   'source': {
-//     'type': 'raster',
-//     'tiles': [
-//       'https://geodata.state.nj.us/imagerywms/Natural2015?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=Natural2015'
-//     ],
-//     'tileSize': 256
-//   },
-//   'paint': {}
-//   },'aeroway-taxiway');
-// });
+// // Adds a WMS (tested, maybe modify later)
+// // Use MBGL exmaple https://www.mapbox.com/mapbox-gl-js/example/wms/
+// // map.on('load', function() {
+// // map.addLayer({
+// //   'id':'wms-test-layer',
+// //   'type': 'raster',
+// //   'source': {
+// //     'type': 'raster',
+// //     'tiles': [
+// //       'https://geodata.state.nj.us/imagerywms/Natural2015?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=Natural2015'
+// //     ],
+// //     'tileSize': 256
+// //   },
+// //   'paint': {}
+// //   },'aeroway-taxiway');
+// // });
 
 
 
-// From MB-GL draw example
-map.on('draw.create', updateArea);
-map.on('draw.delete', updateArea);
-map.on('draw.update', updateArea);
+// // From MB-GL draw example
+// map.on('draw.create', updateArea);
+// map.on('draw.delete', updateArea);
+// map.on('draw.update', updateArea);
 
-// Use lit-html to return polygon area or no area message
-function updateArea(e) {
-  let data = draw.getAll();
+// // Use lit-html to return polygon area or no area message
+// function updateArea(e) {
+//   let data = draw.getAll();
 
-  let areaMessage;
-  if (data.features.length > 0) {
-    let area = turf.area(data);
-    let rounded_area = Math.round(area*100)/100;
-    areaMessage = html`<p>The area is ${rounded_area} square meters </p>`
-    console.log('data type of; ', typeof data);
-    // let intersectData = turf.intersect(data, polyLarge);
-    // console.log('intersectData type of; ', typeof intersectData);
-  } else {
-    areaMessage = html`<p>No area selected</p>`
-  };  
+//   let areaMessage;
+//   if (data.features.length > 0) {
+//     let area = turf.area(data);
+//     let rounded_area = Math.round(area*100)/100;
+//     areaMessage = html`<p>The area is ${rounded_area} square meters </p>`
+//     console.log('data type of; ', typeof data);
+//     // let intersectData = turf.intersect(data, polyLarge);
+//     // console.log('intersectData type of; ', typeof intersectData);
+//   } else {
+//     areaMessage = html`<p>No area selected</p>`
+//   };  
 
-  const areaTemplate = html`${areaMessage}`;
-  render(areaTemplate, outcomes__area);
+//   const areaTemplate = html`${areaMessage}`;
+//   render(areaTemplate, outcomes__area);
 
-};
+// };
 
 
 
-// Practice polygons CNPA
+// // Practice polygons CNPA (working)
 
           
           
           
-          const polyLarge = turf.polygon([[
-            [
-              -3.922119140625,
-              56.88650220638941
-            ],
-            [
-              -3.409881591796875,
-              56.88650220638941
-            ],
-            [
-              -3.409881591796875,
-              57.125804948722575
-            ],
-            [
-              -3.922119140625,
-              57.125804948722575
-            ],
-            [
-              -3.922119140625,
-              56.88650220638941
-            ]
-          ]]);
+//           const polyLarge = turf.polygon([[
+//             [
+//               -3.922119140625,
+//               56.88650220638941
+//             ],
+//             [
+//               -3.409881591796875,
+//               56.88650220638941
+//             ],
+//             [
+//               -3.409881591796875,
+//               57.125804948722575
+//             ],
+//             [
+//               -3.922119140625,
+//               57.125804948722575
+//             ],
+//             [
+//               -3.922119140625,
+//               56.88650220638941
+//             ]
+//           ]]);
           
           
-          const polyThin = turf.polygon([[
-            [
-              -4.069061279296875,
-              57.025036816645255
-            ],
-            [
-              -3.2148742675781246,
-              57.025036816645255
-            ],
-            [
-              -3.2148742675781246,
-              57.060150113451755
-            ],
-            [
-              -4.069061279296875,
-              57.060150113451755
-            ],
-            [
-              -4.069061279296875,
-              57.025036816645255
-            ]
-          ]]);
+//           const polyThin = turf.polygon([[
+//             [
+//               -4.069061279296875,
+//               57.025036816645255
+//             ],
+//             [
+//               -3.2148742675781246,
+//               57.025036816645255
+//             ],
+//             [
+//               -3.2148742675781246,
+//               57.060150113451755
+//             ],
+//             [
+//               -4.069061279296875,
+//               57.060150113451755
+//             ],
+//             [
+//               -4.069061279296875,
+//               57.025036816645255
+//             ]
+//           ]]);
 
 
 
-console.log('polyLarge type: ',typeof polyLarge);
-console.log('polyThin type: ',typeof polyThin);
+// console.log('polyLarge type: ',typeof polyLarge);
+// console.log('polyThin type: ',typeof polyThin);
 
 
 
 
-// Turf intersection test
-let intersectTest = intersect(polyLarge, polyThin);
-console.log('in type intersectTest: ',typeof intersectTest);
+// Turf intersection test (working)
+// let intersectTest = intersect(polyLarge, polyThin);
+// console.log('in type intersectTest: ',typeof intersectTest);
 
 
 
