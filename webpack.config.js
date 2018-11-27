7c7b8ad4fa857130e83a2f921cd693826d36c5df
Kit Macleod <kitmacleod@gmail.com>
2018-11-27 18:27:07 +0000

    Removed devtool option from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = (env) => {
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
-    },
-    devtool: isProduction ? 'source-map':'cheap-module-eval-source-map',
+    }
+   
     devServer:{
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
