3dc864af4a7d747ebc442f203d4f8a94175179b1
Kit Macleod <kitmacleod@gmail.com>
2019-01-24 10:32:55 +0000

    Trying to fix css not updating, rolled back config, but still not updating

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
- const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const devMode = process.env.NODE_ENV !== 'production'
 
 // TODO 
@@ -9,33 +9,36 @@ const devMode = process.env.NODE_ENV !== 'production'
 // Should have separate config files
 
   module.exports ={
-    entry: {
-      app: './src/index.js',
-    },
+    // entry: {
+      // app: './src/index.js',
+    entry: './src/index.js',  
+    // },
     output: {
       path: path.join(__dirname, 'public', 'dist'),
-      filename: '[name].bundle.js'
+      filename: 'bundle.js'
     },
     mode: devMode ? 'development':'production',
-     optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    },
+    //  optimization: {
+    //   splitChunks: {
+    //     chunks: 'all'
+    //   }
+    // },
    
     devtool: devMode ? 'cheap-module-eval-source-map': 'source-map',
     devServer:{
+      hot: true,
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath:'/dist/'
+      //publicPath: path.join(__dirname, 'public', 'dist')
+       publicPath:'/dist/'
     },
     module: {
       rules: [
         {
-          //test: /\.s?css$/,
-          test:/\.(sa|sc|c)ss$/,
+          test: /\.s?css$/,
+         // test:/\.(sa|sc|c)ss$/,
           use: [
-            devMode ? 'style-loader' : MiniCssExtractPlugin,
+          //  devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
             'style-loader',
             'css-loader',
             'sass-loader'
@@ -43,22 +46,22 @@ const devMode = process.env.NODE_ENV !== 'production'
         }]
     },
     plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'public/index.html',
-        chunks: ['app']
-      }),
-      new HtmlWebpackPlugin({
-        filename: 'map2.html',
-        template: 'public/map2.html', 
-        chunks: ['app']
-      }),
-      new MiniCssExtractPlugin({
-        filename: devMode ? '[name].css': '[name].[contenthash].css',
-        chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css'
+      new CleanWebpackPlugin(['dist'])
+      // new HtmlWebpackPlugin({
+      //   filename: 'index.html',
+      //   template: 'public/index.html',
+      //   chunks: ['app']
+      // }),
+      // new HtmlWebpackPlugin({
+      //   filename: 'map2.html',
+      //   template: 'public/map2.html', 
+      //   chunks: ['app']
+      // })
+      // new MiniCssExtractPlugin({
+      //   filename: devMode ? '[name].css': '[name].[contenthash].css',
+      //   chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css'
 
-      })
+      // })
   
     ],
     node: {
