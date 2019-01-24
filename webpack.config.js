e7f572e4921173b6e25295e7bcb2735446991b8d
Kit Macleod <kitmacleod@gmail.com>
2019-01-24 13:31:54 +0000

    Trying to fix css in webpack config, gone back to 271118, was working, now not

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,113 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production'
+// const CopyPlugin = require('copy-webpack-plugin');
 
-// TODO 
-// May need uglify JS
-// Should have separate config files
 
-  module.exports ={
-    // entry: {
-      // app: './src/index.js',
-    entry: './src/index.js',  
-    // },
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry:'./src/index.js',
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
-    mode: devMode ? 'development':'production',
-    //  optimization: {
-    //   splitChunks: {
-    //     chunks: 'all'
-    //   }
-    // },
-   
-    devtool: devMode ? 'cheap-module-eval-source-map': 'source-map',
+    devtool: isProduction ? 'source-map':'cheap-module-eval-source-map',
     devServer:{
-      hot: true,
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      //publicPath: path.join(__dirname, 'public', 'dist')
-       publicPath:'/dist/'
+      publicPath:'/dist/'
     },
     module: {
       rules: [
         {
           test: /\.s?css$/,
-         // test:/\.(sa|sc|c)ss$/,
           use: [
-          //  devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
             'style-loader',
             'css-loader',
             'sass-loader'
           ]
         }]
     },
-    plugins: [
-      new CleanWebpackPlugin(['dist'])
-      // new HtmlWebpackPlugin({
-      //   filename: 'index.html',
-      //   template: 'public/index.html',
-      //   chunks: ['app']
-      // }),
-      // new HtmlWebpackPlugin({
-      //   filename: 'map2.html',
-      //   template: 'public/map2.html', 
-      //   chunks: ['app']
-      // })
-      // new MiniCssExtractPlugin({
-      //   filename: devMode ? '[name].css': '[name].[contenthash].css',
-      //   chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css'
-
-      // })
-  
-    ],
     node: {
       fs: "empty"
     }
   };
-//};
+};
+
+
+
+
+
+//--240119 Old config, not refreshing scss, so used earlier config--
+// const path = require('path');
+// // const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const devMode = process.env.NODE_ENV !== 'production'
+
+// // TODO 
+// // May need uglify JS
+// // Should have separate config files
+
+//   module.exports ={
+//     // entry: {
+//       // app: './src/index.js',
+//     entry: './src/index.js',  
+//     // },
+//     output: {
+//       path: path.join(__dirname, 'public', 'dist'),
+//       filename: 'bundle.js'
+//     },
+//     mode: devMode ? 'development':'production',
+//     //  optimization: {
+//     //   splitChunks: {
+//     //     chunks: 'all'
+//     //   }
+//     // },
+   
+//     devtool: devMode ? 'cheap-module-eval-source-map': 'source-map',
+//     devServer:{
+//       hot: true,
+//       contentBase: path.join(__dirname, 'public'),
+//       historyApiFallback: true,
+//       //publicPath: path.join(__dirname, 'public', 'dist')
+//        publicPath:'/dist/'
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.s?css$/,
+//          // test:/\.(sa|sc|c)ss$/,
+//           use: [
+//           //  devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+//             'style-loader',
+//             'css-loader',
+//             'sass-loader'
+//           ]
+//         }]
+//     },
+//     plugins: [
+//       new CleanWebpackPlugin(['dist'])
+//       // new HtmlWebpackPlugin({
+//       //   filename: 'index.html',
+//       //   template: 'public/index.html',
+//       //   chunks: ['app']
+//       // }),
+//       // new HtmlWebpackPlugin({
+//       //   filename: 'map2.html',
+//       //   template: 'public/map2.html', 
+//       //   chunks: ['app']
+//       // })
+//       // new MiniCssExtractPlugin({
+//       //   filename: devMode ? '[name].css': '[name].[contenthash].css',
+//       //   chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css'
+
+//       // })
+  
+//     ],
+//     node: {
+//       fs: "empty"
+//     }
+//   };
+// //};
 
