da214c1f141b761076d0812bd2e4903697dca501
Kit Macleod <kitmacleod@gmail.com>
2019-02-05 07:04:43 +0000

    Added babel and IE version of layout.css from Autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = (env) => {
     module: {
       rules: [
         {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, {
           test: /\.s?css$/,
           use: [
             'style-loader',
